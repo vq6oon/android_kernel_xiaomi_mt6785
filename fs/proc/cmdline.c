f5700953e2416768655b8550746e7a72b34c3705
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-07-08 11:43:14 +0800

    [BACKPORT] fs: implement SUSFS v1.5.9
    There is a few change in here:
    - sus_mount functionality still remain in v1.5.5 as backporting it to the latest version will result a mount detection leak in some apps/detectors
    
    Thank you @simonpunk for the help of sus_path backport
    
    Co-authored-by: sidex15 <24408329+sidex15@users.noreply.github.com>
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -10,8 +10,18 @@
 static char patched_cmdline[COMMAND_LINE_SIZE];
 #endif
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
 #ifndef CONFIG_PROC_BEGONIA_CMDLINE
 	seq_printf(m, "%s\n", saved_command_line);
 #else
